639ecb24f9f462f825ee25f8226b7a59e159de73
Guilherme Pereira Silva <guilhermepereirasilva2006@gmail.com>
2025-06-02 23:38:23 -0400

    Base do projeto, classes iniciais criadas

---
+++ src/main/java/cba/ifmt/entidades/Usuario.java
@@ -0,0 +1,38 @@
+package cba.ifmt.entidades;
+
+public class Usuario {
+	private int id;
+	private String email;
+	private String cpf;
+	private Municipio municipio;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Municipio getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(Municipio municipio) {
+		this.municipio = municipio;
+	}
+	
+	
+	
+	
+}
